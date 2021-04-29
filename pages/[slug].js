b1a957e677a9cd352cd8cb7fd3da736792812371
David Kang <12dkang@gmail.com>
2021-04-29 13:16:59 -0400

    first commit

---
+++ pages/[slug].js
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+
+export default function Post({ post }) {
+  return (
+    <div>
+      <Link href='/'>
+        <a>Go Home</a>
+      </Link>
+      <h2>{post.title}</h2>
+    </div>
+  )
+}
+
+// tell next.js how many pages there are
+export async function getStaticPaths() {
+  const res = await fetch('http://localhost:1337/posts')
+  const posts = await res.json()
+
+  const paths = posts.map((post) => ({
+    params: { slug: post.slug },
+  }))
+
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+// for each individua page: get the data fro that page
+export async function getStaticProps({ params }) {
+  const { slug } = params
+
+  const res = await fetch(`http://localhost:1337/posts?slug=${slug}`)
+  const data = await res.json()
+  const post = data[0]
+
+  return {
+    props: { post },
+  }
+}
